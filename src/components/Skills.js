32b33be23548df45b35ace7d35888b6e91180c0e
skyreep <skyreep@gmail.com>
2022-01-03 13:53:18 -0500

    Update Content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,9 +12,7 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            With over 5 years of hands-on software development experience, I have the skills to tackle projects across a variety of tech stacks. More importantly, I've learned to quickly parse new technologies in order to quickly and effectively problem solve using any toolset.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
